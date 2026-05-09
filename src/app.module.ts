45209e72615ade2caaec029106c0aa3ad62118b7
Jean-Baptiste V. <jeanbaptistevignonfodo@gmail.com>
2026-05-09 14:45:25 +0100

    Création de module, controller, service pour (organisation auth prisma "uniquement module et service au niveau de prisma") En route pour les endpoint de la V1

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { OrganizationModule } from './organization/organization.module';
+import { AuthModule } from './auth/auth.module';
+import { MembersModule } from './members/members.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [OrganizationModule, AuthModule, MembersModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
