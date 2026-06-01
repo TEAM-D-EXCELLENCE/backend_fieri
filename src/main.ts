adbdcad9c63cec802343e1d6fb5c34d55b319a0a
Octave BAHOUN-HOUTOUKPE <octavebahoun@gmail.com>
2026-06-01 12:11:58 +0100

    AJOUT DU CORS

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,17 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Autorise le frontend local et le frontend de production
+  app.enableCors({
+    origin: [
+      'http://localhost:5173',
+      'https://fier2.vercel.app',
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
